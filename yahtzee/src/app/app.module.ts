e599a0427e5d40e305c8f8424b565efc0d60f1dd
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-07-21 16:39:57 +0300

    mends the app.component, adds styles

--- yahtzee/src/app/app.module.ts
+++ yahtzee/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RollEmComponent } from './components/roll-em/roll-em.component';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
+import { DicePickerComponent } from './components/dice-picker/dice-picker.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RollEmComponent, ScoreboardComponent, DicePickerComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
