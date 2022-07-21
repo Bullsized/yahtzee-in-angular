d9ee8cc6912a542e37a1c7ea030d9c04e3d2c041
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-07-21 16:38:47 +0300

    adds the scoreaboard component

---
+++ yahtzee/src/app/components/scoreboard/scoreboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ekko-scoreboard',
+  templateUrl: './scoreboard.component.html',
+  styleUrls: ['./scoreboard.component.scss'],
+})
+export class ScoreboardComponent {}
